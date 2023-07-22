7a894b11d60eb066190400282c98bc7b3897e80c
0099sakshi <sakshichoudharysc26@gmail.com>
2023-07-22 21:38:24 +0530

    commit change

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var headertitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
-var items=document.getElementsByClassName('list-group-items');
-console.log(items);
-console.log(items[1]);
-items[1].textcontent='hello 2';
-items[1].style.fontweight='bold';
-items[1].style.backgroundcolor='yellow';
+var li=document.getElementsByTagName('li');
+/* header.style.borderBottom='solid 3px #000';
+var items=document.getElementsByClassName('list-group-items');*/
+console.log(li);
+console.log(li[1]);
+li[1].textcontent='hello 2';
+li[1].style.fontweight='bold';
+li[1].style.backgroundcolor='yellow';
 
 for(var i=0; i<items.length; i++){
-    items[1].style.backgroundcolor='green';
+    li[1].style.backgroundcolor='green';
 }
