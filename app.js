521f20aa9c21f86bd34043bc4995e1d3e2f3d2f5
0099sakshi <sakshichoudharysc26@gmail.com>
2023-07-22 21:22:33 +0530

    second time changed

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ header.style.borderBottom='solid 3px #000';
 var items=document.getElementsByClassName('list-group-items');
 console.log(items);
 console.log(items[1]);
-items[1].textcontent='hello';
+items[1].textcontent='hello 2';
 items[1].style.fontweight='bold';
 items[1].style.backgroundcolor='yellow';
+
+for(var i=0; i<items.length; i++){
+    items[1].style.backgroundcolor='green';
+}
