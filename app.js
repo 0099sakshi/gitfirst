de2566fc985cd2d86f965825cbdc5e76bcaceb22
0099sakshi <sakshichoudharysc26@gmail.com>
2023-07-23 20:36:14 +0530

    new commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ for(var i=0; i<items.length; i++){
     li[1].style.backgroundcolor='green';
 }*/
 
-var header=document.querySelector('#main-header');
+/* var header=document.querySelector('#main-header');
 header.Style.borderbottom='solid 4x green'
 
 var input=document.querySelector('input');
@@ -24,4 +24,61 @@ var item=document.querySelector('.list-group-item');
 item.Style.color='green';
 
 var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondtItem.style.color='coral';
+secondtItem.style.color='coral'; */
+
+//parentelements
+console.log(itemlist.parentelement);
+itemlist.parentelement.style.backgroundcolor='#f4f4f4';
+console.log(itemlist.parentelement.parentelement.parentelement);
+
+//childnodes
+console.log(itemlist.children);
+console.log(itemlist.children[1]);
+itemlist.children[1].style.backgroundcolor='yellow';
+
+//firstchild
+console.log(itemlist.firstchild);
+//firstElementchild
+console.log(itemlist.firstElementchild);
+itemlist.firstElementchild.textcontent='hello 1';
+
+//lastchild
+console.log(itemlist.lastchild);
+//lastElementchild
+console.log(itemlist.lastElementchild);
+itemlist.lastElementchild.textcontent='hello 4';
+
+//nextsibling
+console.log(itemlist.nextsibling);
+//nextElementsibling
+console.log(itemlist.nextElementsibling);
+
+//previousSibling
+console.log(itemlist.previousSibling);
+//previousElementSibling
+console.log(itemlist.previousElementSibling);
+itemlist.previousElementSibling.style.backgroundcolor='green';
+
+//createElement
+
+//create a div
+var newDiv=document.createElement('div');
+
+//Add class
+newDiv.classsName='hello 1';
+
+//Add atr
+newDiv.setAttribute('title,helloDiv');
+
+//Create text node
+var newDivText=document.createTextNode('hello world');
+
+//Add text to div
+newDiv.appendChild(newDivText);
+ var container=document.querySelector('header.container');
+ var h1=document.querySelector('header.h1');
+
+ console.log(newDiv);
+
+ newDiv.style.fontsize='30px';
+ container.insertBefore(newDiv,h1);
