e4a34e7c14eb3639e62a214f5dc43a78b4ac9f0d
0099sakshi <sakshichoudharysc26@gmail.com>
2023-07-26 09:51:35 +0530

    new commit change

--- demo.js
+++ demo.js
@@ -50,6 +50,15 @@ function filterItems(e){
     //convert text to lowercase
     var text = e.target.value.toLowerCase();
     console.log(text);
-    itemlist.getElementsByTagName('li');
-    console.log('items');
+    //get lis
+    var items=itemlist.getElementsByTagName('li');
+   //convert to an array
+   Array.from(items).forEach(function(item){
+   var itemName = item.firstchild.textcontent;
+   if(itemName.toLowerCase().indexof(text) != -1){
+    item.style.display = 'block';
+}else{
+    item.style.display = 'none';
+}
+});
 }
